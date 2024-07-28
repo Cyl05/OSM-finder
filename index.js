fd3b6b96112bf7df27ae74ed275363bd64ce0253
R S Chiraag <chiraag.rs@gmail.com>
2024-07-28 19:56:11 +0530

    fixed search (actually works now)

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-
-let coords = [12.96415, 77.58454];
-
-var map = L.map('map').setView(coords, 18);
-// var marker = L.marker(coords).addTo(map);
-
-map.addControl(L.control.search({ position: 'bottomright' }));
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+var map = L.map('map');
+let amenity = "hospital";
+let pincode = "560078";
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Function to perform the search
+function searchLocation(query) {
+    fetch(`https://nominatim.openstreetmap.org/search?q=${query}&bounded=1&format=json`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                    for (let i = 0; i < data.length; i++) {
+                    var location = data[i];
+                    map.setView([location.lat, location.lon], 14);
+                    L.marker([location.lat, location.lon]).addTo(map);
+                }
+            } else {
+                alert("Location not found");
+            }
+        });
+}
+
+// Example usage
+searchLocation(`${amenity}s+near+${pincode}`);
