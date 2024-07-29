2d2f522e729f057f4f3b408fdb3d3a2ef3ff8473
R S Chiraag <chiraag.rs@gmail.com>
2024-07-29 15:55:41 +0530

    Add dynamic zoom when clicked on result, gmaps link button

--- public/app.js
+++ public/app.js
@@ -5,17 +5,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-function listItem (title, type, address) {
+
+function listItem (title, type, address, lat, lon) {
     return `
     <div class="list-element">
         <div class="list-col">
-            <p class="list-title">${title}</p>
+            <p class="list-title" data-lat=${lat} data-lon=${lon}>${title}</p>
             <p class="list-type">${type}</p>
             <p class="list-address">${address}</p>
         </div>
-        <button class="list-button">
-            <img src="pop-in-new.svg">
-        </button>
+        <a href="https://www.google.com/maps/@?api=1&map_action=map&center=${lat},${lon}&zoom=18" target="_blank">
+            <button class="list-button">
+                <img src="pop-in-new.svg">
+            </button>
+        </a>
     </div>
     `;
 }
@@ -24,17 +27,26 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function zoomIntoLoc(loc) {
+    map.setView([loc.data("lat"), loc.data("lon")], 17);
+}
+
 function searchLocation(query) {
     fetch(`https://nominatim.openstreetmap.org/search?q=${query}&bounded=1&format=json`)
         .then(response => response.json())
         .then(data => {
             if (data.length > 0) {
-                    for (let i = 0; i < data.length; i++) {
-                    var location = data[i];
+                for (let i = 0; i < data.length; i++) {
+                    let location = data[i];
                     map.setView([location.lat, location.lon], 14);
-                    L.marker([location.lat, location.lon]).addTo(map);
-                    $("#results").append(listItem(location.name, capitalizeFirstLetter(location.type), location.display_name));
+                    let loc_mark = L.marker([location.lat, location.lon]).addTo(map);
+                    let popUpContent = `<p class="popup-title list-title">${location.name}</p>`;
+                    loc_mark.bindPopup(popUpContent);
+                    $("#results").append(listItem(location.name, capitalizeFirstLetter(location.type), location.display_name, location.lat, location.lon));
                 }
+                $(".list-title").on("click", function() {
+                    zoomIntoLoc($(this))
+                });
             } else {
                 alert("Location not found");
             }
